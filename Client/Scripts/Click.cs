7b07257de44c777dcde4d348d6365fa6c3d9b799
Antoine Giasson <antoine.giasson.2@gmail.com>
2016-01-25 17:43:37 -0500

    Base clicker

---
+++ Client/Scripts/Click.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+    public Text HPDisplay;
+    public int currentHP;
+    public int maxHP = 100;
+    public int damagePerClick = 1;
+    private Slider slider;
+
+    void Start() {
+        currentHP = maxHP;
+        slider = GameObject.Find("HPSlider").GetComponent<Slider>();
+    }
+
+    void Update(){
+        HPDisplay.text = currentHP + "/" + maxHP + " HP";
+        slider.value = (currentHP * 100) / maxHP;
+    }
+
+
+    public void Clicked(){
+        if (currentHP != 0){
+            currentHP -= damagePerClick;
+        }
+    }
+
+}
