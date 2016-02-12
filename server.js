0cd16ed1ca02a594315c5017be3e1208ba922f72
sapodius <martin0savoie@gmail.com>
2016-02-12 15:17:30 -0500

    Removing some useless stuff and changing project name

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-require('newrelic');
 var express = require('express');
 var app = express();
 var http = require('http');
 var ws = require('ws');
 var WebSocketServer = require("ws").Server
-
-//io.on('connection', chat.onConnection);
-//
-//http.listen(process.env.PORT || 3000);
-//console.log('App started at port 3000!!!');
 var port = process.env.PORT || 3000;
 var server = http.createServer(app)
+
 server.listen(port)
 
 console.log("http server listening on %d", port)
@@ -35,17 +30,4 @@ function onNewMessage(socket) {
     if (socket.readyState == ws.OPEN) {
         socket.send("Pi is " + pi);
     }
-}
-
-function estimatePi() {
-    var n = 10000000, inside = 0, i, x, y;
-
-    for (i = 0; i < n; i++) {
-        x = Math.random();
-        y = Math.random();
-        if (Math.sqrt(x * x + y * y) <= 1)
-            inside++;
-    }
-
-    return 4 * inside / n;
 }
