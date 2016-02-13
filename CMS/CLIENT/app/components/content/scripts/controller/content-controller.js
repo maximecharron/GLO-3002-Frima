7b6d24a36250c7ffdbaa864f880f874501bd9657
Isaac <provenchez@gmail.com>
2016-02-13 12:14:43 -0500

    Ajout des calls au CMS serveur avec le body seulement le chiffre changé

--- CMS/CLIENT/app/components/content/scripts/controller/content-controller.js
+++ CMS/CLIENT/app/components/content/scripts/controller/content-controller.js
@@ -1,13 +1,16 @@
-ContentApp.controller("content-controller", function ($scope) {
+ContentApp.controller("content-controller", function ($scope, $http) {
 
     <!-- $scope déclare les variables utilisées dans le html -->
+
+    var cmsUrl = "http://frima-cms-server.herokuapp.com/update"
+
     $scope.updateMonsterHealth = function ()
     {
         if(!$scope.monsterHealth){
             window.alert( "not a valid number" );
         }
         else{
-            window.alert( $scope.monsterHealth );
+            makeCorsRequest('POST', cmsUrl, $scope.monsterHealth);
         }
     }
 
@@ -17,7 +20,7 @@ ContentApp.controller("content-controller", function ($scope) {
             window.alert( "not a valid number" );
         }
         else{
-            window.alert( $scope.monsterSpeed);
+            makeCorsRequest('POST', cmsUrl, $scope.monsterSpeed);
         }
 
     }
@@ -29,8 +32,54 @@ ContentApp.controller("content-controller", function ($scope) {
         }
         else{
             window.alert( $scope.monsterArmor);
+            makeCorsRequest('POST', cmsUrl, $scope.monsterSpeed);
         }
 
     }
 
+    function successCallback(){
+        window.alert( "Success Callback" );
+    }
+
+    function errorCallback(){
+        window.alert( "Success Callback" );
+    }
+
+    function createCORSRequest(method, url) {
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+            xhr.open(method, url, true);
+
+        } else if (typeof XDomainRequest != "undefined") {
+
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+
+        } else {
+
+            xhr = null;
+
+        }
+        return xhr;
+    }
+
+    function makeCorsRequest(request, url, body) {
+
+        var xhr = createCORSRequest('POST', url);
+
+        if (!xhr) {
+            alert('CORS not supported');
+            return;
+        }
+        xhr.onload = function() {
+            var text = xhr.responseText;
+            alert('Response from CORS request to ' + url + ': '+ text);
+        };
+
+        xhr.onerror = function() {
+            alert('Woops, there was an error making the request.');
+        };
+
+        xhr.send(body);
+    }
 })
