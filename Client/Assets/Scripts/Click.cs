53eb0d6c9c7d05a080a098411362dd00a5d27a6e
Antoine Giasson <antoine.giasson.2@gmail.com>
2016-02-13 01:20:53 -0500

    WebSocket + connection server

--- Client/Assets/Scripts/Click.cs
+++ Client/Assets/Scripts/Click.cs
@@ -1,29 +1,43 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
+using System.Text.RegularExpressions;
 
 public class Click : MonoBehaviour {
 
     public Text HPDisplay;
-    public int currentHP;
-    public int maxHP = 100;
-    public int damagePerClick = 1;
+    public long currentHP;
+    public long maxHP = 10000000000;
     private Slider slider;
+    public WebSocket w;
 
-    void Start() {
+    void Start()
+    {
         currentHP = maxHP;
         slider = GameObject.Find("HPSlider").GetComponent<Slider>();
+        w = new WebSocket(new Uri("ws://frima-server-1.herokuapp.com"));
+        StartCoroutine(w.Connect());
     }
 
-    void Update(){
-        HPDisplay.text = currentHP + "/" + maxHP + " HP";
+    void Update()
+    {
+        string reply = w.RecvString();
+        if (reply != null)
+        {
+            string hp = Regex.Match(reply, @"\d+").Value;
+            currentHP = Int64.Parse(hp);
+        }
+        HPDisplay.text = currentHP + " HP";
         slider.value = (currentHP * 100) / maxHP;
     }
 
 
-    public void Clicked(){
-        if (currentHP != 0){
-            currentHP -= damagePerClick;
+    public void Clicked()
+    {
+        if (currentHP != 0)
+        {
+            w.SendString("Meurt");
         }
     }
 
