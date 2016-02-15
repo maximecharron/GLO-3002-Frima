321eec95beabb9e8778eda3d2e63ec3b4b4ef691
sapodius <martin0savoie@gmail.com>
2016-02-15 18:20:29 -0500

    Adding Redis for CMS

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ var corsOptions = {
 };
 var theUrl = 'redis://h:p88th5goahehq8c9hta4ugr533t@ec2-54-227-246-40.compute-1.amazonaws.com:21599';
 
-var rtg = require('url').parse(theUrl);
-var redis = require('redis').createClient(rtg);
-redis.auth(rtg.auth.split(":")[1]);
-
+//var rtg = require('url').parse(theUrl);
+var redis = require('redis').createClient(theUrl);
+//redis.auth(rtg.auth.split(":")[1]);
+redis.auth('password', function(err){})
 require('./middleware/passport')(passport, app);
 
 app.use(cookieParser());
