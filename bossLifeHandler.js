b3e7f753ecd1e809809000ff8317294c49273c55
sapodius <martin0savoie@gmail.com>
2016-02-15 15:11:30 -0500

    developping with heroku

---
+++ bossLifeHandler.js
@@ -0,0 +1,59 @@
+exports.getLife = function()
+{
+    async.waterfall([
+        function(currentBossLifeCallBack){getCurrentBossLife(currentBossLifeCallBack)},
+        function(constantBossLifeCallBack){getConstantBossLife(constantBossLifeCallBack)}
+    ]);
+}
+
+function getCurrentBossLife (currentBossLifeCallBack)
+{
+    redisValuesHandler.get('currentBossLife',function(error, result)
+    {
+        if(error)
+        {
+            console.log("Error getting currentBossLife: ", error);
+        }
+        if(!result)
+        {
+            console.log("currentBossLife is null or empty.");
+            currentBossLifeCallBack(null);
+        }
+        else
+        {
+            bossLife = result;
+            console.log("currentBossLife: ", bossLife)
+            currentBossLifeCallBack(null);
+        }
+    });
+}
+
+function getConstantBossLife(constantBossLifeCallBack)
+{
+    if(!bossLife)
+    {
+        redisValuesHandler.get('constantBossLife',function(error, result)
+        {
+            if(error)
+            {
+                console.log("Error getting constantBossLife: ", error);
+            }
+            if(!result)
+            {
+                console.log("constantBossLife is null or empty.");
+                bossLife = 10000000; //TODO: A modifier car ne devrais pas harcoder de valeur. Devrais aller rechercher en BD Mongo les valeurs de la partie.
+                constantBossLifeCallBack(null);
+            }
+            else
+            {
+                bossLife = result;
+                console.log("constantBossLife: ", bossLife)
+                constantBossLifeCallBack(null);
+            }
+        })
+    }
+    else
+    {
+        constantBossLifeCallBack(null);
+    }
+}
