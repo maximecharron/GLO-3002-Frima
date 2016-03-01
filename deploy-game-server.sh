2fbedc2e97d9b2f5ca3f6fd433c657f2dd6f51f8
Maxime Charron <maxime.charron.4@ulaval.ca>
2016-03-01 10:44:33 -0500

    Trying autodeploy on test repo

--- deploy-game-server.sh
+++ deploy-game-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-
-git remote add frima-server-test https://git.heroku.com/frima-server-test.git || exit 1;
-
-git subtree push --prefix GameServer frima-server-test master || exit 1;
+echo "Did 1"
+git remote add frima-server-test https://git.heroku.com/frima-server-test.git || exit 1
+echo "Did 2"
+git subtree push --prefix GameServer frima-server-test master || exit 1
+echo "Did 3"
