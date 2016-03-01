5fbde608c49da6b982b1462572df267b6474e9b9
Maxime Charron <maxime.charron.4@ulaval.ca>
2016-03-01 11:03:34 -0500

    Trying autodeploy on test repo

--- deploy-game-server.sh
+++ deploy-game-server.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-echo "In it"
-set -e
-echo "Did 1"
-git remote add frima-server-test https://git.heroku.com/frima-server-test.git || exit 1
-echo "Did 2"
-git subtree push --prefix GameServer frima-server-test master || exit 1
+gem install heroku
+# Add your Heroku git repo:
+git remote add heroku git@heroku.com:frima-server-test.git
+  # Turn off warnings about SSH keys:
+echo "Host heroku.com" >> ~/.ssh/config
+echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+echo "   CheckHostIP no" >> ~/.ssh/config
+echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+  # Clear your current Heroku SSH keys:
+heroku keys:clear
+  # Add a new SSH key to Heroku
+yes | heroku keys:add
+  # Push to Heroku!
+yes | git subtree push --prefix GameServer heroku master || exit 1
 echo "Did 3"
