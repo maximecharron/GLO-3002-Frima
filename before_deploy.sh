1e5b4c351815672dbb66b2f05f9957c55a7776ce
Maxime Charron <maxime.charron.4@ulaval.ca>
2016-03-01 12:03:11 -0500

    Trying autodeploy on test repo

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ $TRAVIS_BRANCH == 'server_release' ]]; then
+if [[ "$TRAVIS_BRANCH" == "server_release" ]]; then
   cd GameServer
 else
-    echo "else"
+    echo "$TRAVIS_BRANCH"
 fi
