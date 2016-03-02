a3d91601e34eb9bf513d324d9f196bdcfef54bde
Olivier Robert <o.robert@hotmail.com>
2016-03-02 00:28:20 -0500

    Started the Unity project structure with title scene

---
+++ Client/Unity Project/Assets/Scripts/ClickOLD.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using System.Text.RegularExpressions;
+
+public class Click : MonoBehaviour {
+
+    public Text HPDisplay;
+    public long currentHP;
+    public long maxHP;
+    public WebSocket webSocket;
+    private Slider hpSlider;
+    private Button bossButton;
+    private bool firstHP = true;
+    private string serverAddress = "wss://frima-server-1.herokuapp.com";
+
+    void Start(){
+        bossButton = GameObject.Find("Click").GetComponent<Button>();
+        bossButton.interactable = false;
+        hpSlider = GameObject.Find("HPSlider").GetComponent<Slider>();
+        hpSlider.value = 0;
+        webSocket = new WebSocket(new Uri(serverAddress));
+        StartCoroutine(webSocket.Connect());
+        InvokeRepeating("KeepConnectionAlive", 1f, 30f);
+        InvokeRepeating("UpdateLife", 2f, 0.02f);     
+    }
+
+    void UpdateLife(){
+        string reply = webSocket.RecvString();
+        if (reply != null){
+            if (firstHP == true){
+                maxHP = Int64.Parse(reply);
+                currentHP = maxHP;
+                firstHP = false;
+                bossButton.interactable = true;
+            }
+            else{
+                currentHP = Int64.Parse(reply);
+            }
+            HPDisplay.text = currentHP + " HP";
+            hpSlider.value = (currentHP * 100) / maxHP;
+        }
+    }
+
+    public void Clicked(){
+        if (currentHP != 0){
+            webSocket.SendString("Attack");
+        }
+    }
+
+    void KeepConnectionAlive(){
+        webSocket.SendString("Poke");
+    }
+
+}
---
+++ Client/Unity Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class GameController : MonoBehaviour
+    {
+        //Configurable script parameters
+        public CommunicationService communicationService;
+
+        //Privates
+
+        void Awake()
+        {
+            DontDestroyOnLoad(transform.gameObject);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
