6fd143fedf3b3d4ca081379aa6de9e1309c3fd8b
Isaac <provenchez@gmail.com>
2016-03-02 12:43:19 -0500

    Projet de test pour le c#

---
+++ Client/Unity Project/Assets/Scripts/Tests/UnityTestProject/UnityTestProject/CommunicationServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnityTestProject
+{
+    [TestClass]
+    public class CommunicationServiceTests
+    {
+        [TestMethod]
+        public void TestStart() {
+            
+        }
+    }
+}
---
+++ Client/Unity Project/Assets/Scripts/Tests/UnityTestProject/UnityTestProject/GameControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnityTestProject
+{
+    [TestClass]
+    public class GameControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
