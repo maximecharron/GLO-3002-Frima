525d24b4ef3c60864cab884ff831bb86cb82bacd
Isaac <provenchez@gmail.com>
2016-03-02 16:29:41 -0500

    Ajout du testing tool de unity

---
+++ Client/Unity Project/Assets/Tests/Editor/GameControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+
+namespace Assets.Scripts
+{
+    public class GameControllerTest
+    {
+        GameController gameController;
+
+        [Test]
+        public void basicTest()
+        {
+            gameController = new GameController();
+            //Arrange
+            var gameObject = new GameObject();
+
+            //Act
+            //Try to rename the GameObject
+            var newGameObjectName = "My game object";
+            gameObject.name = newGameObjectName;
+
+            //Assert
+            //The object has a new name
+            Assert.AreEqual(newGameObjectName, gameObject.name);
+        }
+    }
+}
