0589407321c7c13237be4d74e80931d5805957ab
Isaac <provenchez@gmail.com>
2016-03-02 20:44:06 -0500

    Ajout du Victory screen

---
+++ Client/Unity Project/Assets/Scripts/Scenes/VictoryScreenController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+namespace Assets.Scripts
+{
+    public class VictoryScreenController : SceneController
+    {
+
+        public EventSystem eventSystem;
+        public GameObject PlayAgainButton;
+        public GameObject MenuButton;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            /*
+            if (eventSystem.currentSelectedGameObject == PlayAgainButton)
+            {
+                SceneManager.LoadScene(GAME_SCENE_NAME);
+            }*/
+
+            if (eventSystem.currentSelectedGameObject == MenuButton)
+            {
+                SceneManager.LoadScene(MENU_SCENE_NAME);
+            }
+        }
+    }
+}
---
+++ Client/Unity Project/Assets/Tests/Editor/VictoryScreemControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+
+namespace Assets.Scripts
+{
+
+    public class VictoryScreemControllerTest
+    {
+
+        VictoryScreenController victoryScreenController;
+
+        [Test]
+        public void basicTestVictoryScreemController()
+        {
+
+            //Arrange
+            var gameObject = new GameObject();
+            gameObject.AddComponent<VictoryScreenController>();
+            victoryScreenController = gameObject.GetComponent<VictoryScreenController>();
+
+            //Act
+            //Try to rename the GameObject
+            var newGameObjectName = "My game object";
+            gameObject.name = newGameObjectName;
+
+            //Assert
+            //The object has a new name
+            Assert.AreEqual(newGameObjectName, gameObject.name);
+        }
+    }
+}
