4b6a5a29f2f681c98f9852c45e1b5962723edc15
Antoine Giasson <antoine.giasson.2@gmail.com>
2016-03-02 20:44:47 -0500

    Game scene first part

---
+++ Client/Unity Project/Assets/Scripts/Scenes/GameSceneController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+
+    public class GameSceneController : SceneController
+    {
+
+        // Configurable script parameters
+        public EventSystem eventSystem;
+        public GameObject exitButton;
+
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (eventSystem.currentSelectedGameObject == exitButton) {
+                SceneManager.LoadScene(MENU_SCENE_NAME);
+            }
+        }
+    }
+}
