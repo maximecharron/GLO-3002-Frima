6c489d5c7b07f88066c3ef847fb529a65784536c
Olivier Robert <o.robert@hotmail.com>
2016-03-28 09:37:13 -0400

    CommandInterceptors for command batching

---
+++ Client/Unity Project/Assets/Scripts/Communication/ICommandInterceptor.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Scripts.Communication.CommandDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Communication
+{
+    public interface ICommandInterceptor
+    {
+        bool ReceiveIntercept(CommandDTO commandDTO);
+
+        bool SendIntercept(CommandDTO commandDTO);
+    }
+}
