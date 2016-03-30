924dff3da2332f8a15f55433a997cc1ff2fd9652
Maxime Charron <maxime.charron.4@ulaval.ca>
2016-03-30 10:17:19 -0400

    Revert client

---
+++ Client/Unity Project/Assets/Scripts/Scenes/TitleSceneController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Scenes
+{
+
+    public class TitleSceneController : SceneController {
+
+        public void OnLoginButtonPointerClick()
+        {
+            SceneManager.LoadScene(LOGIN_SCENE_NAME);
+        }
+
+        public void OnRegisterButtonPointerClick()
+        {
+            SceneManager.LoadScene(REGISTRATION_SCENE_NAME);
+        }
+
+        public void OnExitButtonPointerClick()
+        {
+            UnityEngine.Application.Quit();
+        }
+
+    }
+}
