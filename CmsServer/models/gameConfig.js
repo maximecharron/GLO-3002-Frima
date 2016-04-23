21706b2f9e0b12ebb6adf6ca871150863c493d67
Maxime Charron <charron.maxime97@gmail.com>
2016-04-23 13:29:10 -0400

    Added new config calls

--- CmsServer/models/gameConfig.js
+++ CmsServer/models/gameConfig.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose');
 var modelHelpers = require('./modelHelpers.js');
 
-var gameConfigSchema = new mongoose.Schema();
-
-gameConfigSchema.add({
+var gameConfigSchema = new mongoose.Schema({
     baseAttackDamage : Number,
     baseExperienceIncreaseOnHit : Number,
-    hypeAttackDamage : Number
-});
+    hypeAttackDamage : Number,
+    maximumLevel : Number,
+    levelXPTree: [Number],
+    upgradePointsPerLevel : [Number],
+    probabilityLoot : [Number]
+}, {strict: false});
 
 gameConfigSchema.method('toJSON', modelHelpers.toJSON);
 
