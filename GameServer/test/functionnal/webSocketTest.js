891eef91f3733f5f402afb03347b430338379de5
sapodius <martin0savoie@gmail.com>
2016-04-27 17:16:58 -0400

    Nearly complete functionnal test! Need only one more and all done

--- GameServer/test/functionnal/webSocketTest.js
+++ GameServer/test/functionnal/webSocketTest.js
@@ -3,6 +3,7 @@ var redis = require('redis').createClient(process.env.REDIS_URL);
 var GameConfig = require('./../../models/gameConfig.js').model;
 var Combo = require('./../../models/combo.js').model;
 var User = require('./../../models/user.js').model;
+var Item = require('./../../models/item.js').model;
 var expect = require("chai").expect;
 var assert = require('chai').assert;
 var WebSocket = require('ws');
@@ -32,15 +33,26 @@ describe("Functionnal webSocket", function ()
             webSocketClient.on("message", function(message)
             {
                 //Arrange
+                var json;
                 var expectedCommand = "bossStatusUpdate";
 
                 //Act
-                var json = JSON.parse(message);
+                webSocketClient.on("message", function(message)
+                {
+                    var messageParsed = JSON.parse(message);
+                    if(messageParsed.command.name == expectedCommand ){
+                        json = messageParsed;
+                    }
+                });
 
                 //Assert
-                expect(expectedCommand).to.equal(json.command.name);
-                onConnectCurrentBossLife = json.command.parameters.currentBossLife;
-                done();
+
+                setTimeout(function()
+                {
+                    assert.equal(expectedCommand, json.command.name);
+                    onConnectCurrentBossLife = json.command.parameters.currentBossLife;
+                    done();
+                }, 2000);
             });
         });
     });
@@ -70,35 +82,85 @@ describe("Functionnal webSocket", function ()
                 webSocketClient.send(JSON.stringify(jsonAttack));
             };
 
-            var lowerThan;
-            var bossLife;
+            var expectedLowerThan = true;
+            var lowerThan = false;
+
+            var json;
+            var expectedCommand = "bossStatusUpdate";
 
             webSocketClient.on("message", function(message)
             {
-                var json = JSON.parse(message);
-
-                bossLife = json.command.parameters.currentBossLife
-                lowerThan = bossLife < onConnectCurrentBossLife;
+                var messageParsed = JSON.parse(message);
+                if(messageParsed.command.name == expectedCommand && messageParsed.command.parameters.currentBossLife < onConnectCurrentBossLife ){
+                    json = messageParsed;
+                    lowerThan = true;
+                }
             });
 
             //Assert
             setTimeout(function()
             {
-                assert.equal(true, lowerThan);
+                assert.equal(expectedLowerThan, lowerThan);
                 done();
-            }, 1000);
+            }, 2000);
 
         });
     });
 
     describe('makeDamage to kill boss', function()
     {
-        it('should receive message bossStatusUpdate with status dead', function(done)
-        {
-            //Arrange
-            this.timeout(5000);
+        var tokenSecret = process.env.TOKEN_SECRET || 'FRIMA_TOKEN_SECRET';
+        var user;
+        var registerClient;
+        var gameConfigModel;
+        var item;
+        var jsonAttack;
 
-            var jsonAttack =
+        beforeEach(function (done)
+        {
+            item = new Item();
+            item.type = 0;
+            item.subType = 0;
+            item.name = "Simple Adrenaline Shot";
+            item.quantity = 0;
+            item.staminaRegeneration = 0;
+            item.hypeGeneration = 2;
+            item.effectDuration = 30;
+
+            gameConfigModel = new GameConfig();
+            gameConfigModel.baseAttackDamage = 2;
+            gameConfigModel.baseExperienceIncreaseOnHit = 2;
+            gameConfigModel.hypeAttackDamage = 2222;
+            gameConfigModel.maximumLevel = 2;
+            gameConfigModel.probabilityLoot = [1,1,1,1];
+
+
+            user = new User();
+            user.id = 123456789;
+            user.username = "Test";
+            user.email = "test@test.ca";
+            user.items = [];
+
+            var expires = moment().add(1, 'days').valueOf();
+            user.token = jwt.encode(
+                {
+                    iss: user.id,
+                    exp: expires
+                },
+                tokenSecret
+            );
+
+            registerClient =
+            {
+                "command" : {
+                    "name" : "registerClient",
+                    "parameters" : {
+                        "token" : user.token
+                    }
+                }
+            };
+
+            jsonAttack =
             {
                 command:
                 {
@@ -110,9 +172,54 @@ describe("Functionnal webSocket", function ()
                 }
             };
 
+            user.save(function(err){
+                if(err){
+                    console.log("Error to save user in functionnal test: ", err);
+                }
+                gameConfigModel.save(function (err) {
+                    if (err) {
+                        console.log("Error to save gameConfig in functionnal test: ", err);
+                    }
+                    redis.publish("gameConfigUpdate", "update");
+                    item.save(function(err) {
+                        if (err) {
+                            console.log("Error to save item in functionnal test: ", err);
+                        }
+                        redis.publish("itemsUpdate", "update");
+                        done();
+                    });
+                });
+            });
+        });
+
+        afterEach(function(done){
+            User.remove({ }, function (err) {
+                if (err){
+                    console.log("Problem to Remove user!: ", err);
+                }
+                GameConfig.remove({ }, function (err) {
+                    if (err){
+                        console.log("Problem to Remove gameConfig!: ", err);
+                    }
+                    Item.remove({ }, function (err) {
+                        if (err){
+                            console.log("Problem to Remove item!: ", err);
+                        }
+                        done();
+                    });
+                });
+            });
+        });
+
+        it('should receive message bossStatusUpdate with status dead', function(done)
+        {
+            //Arrange
+            this.timeout(5000);
+
             //Act
             webSocketClient.onopen = function()
             {
+                webSocketClient.send(JSON.stringify(registerClient));
                 webSocketClient.send(JSON.stringify(jsonAttack));
             };
 
@@ -132,9 +239,74 @@ describe("Functionnal webSocket", function ()
             {
                 assert.equal(expectedStatus, json.command.parameters.status);
                 done();
-            }, 1000);
+            }, 2000);
 
         });
+
+        it('should receive message bossStatusUpdate with status alive', function(done)
+        {
+            //Arrange
+            this.timeout(5000);
+
+            //Act
+            webSocketClient.onopen = function()
+            {
+                webSocketClient.send(JSON.stringify(registerClient));
+                webSocketClient.send(JSON.stringify(jsonAttack));
+            };
+
+            var json;
+            var expectedStatus = 0;
+            webSocketClient.on("message", function(message)
+            {
+                var messageParsed = JSON.parse(message);
+                if(messageParsed.command.name == "bossStatusUpdate" && messageParsed.command.parameters.status == expectedStatus){
+                    json = messageParsed;
+                }
+            });
+
+            //Assert
+
+            setTimeout(function()
+            {
+                assert.equal(expectedStatus, json.command.parameters.status);
+                done();
+            }, 2000);
+
+        });
+
+        it('should receive message lootItems', function(done)
+        {
+            //Arrange
+            this.timeout(5000);
+
+            //Act
+            webSocketClient.onopen = function()
+            {
+                webSocketClient.send(JSON.stringify(registerClient));
+                webSocketClient.send(JSON.stringify(jsonAttack));
+            };
+
+            var json;
+            var expectedCommand = "lootItems";
+            webSocketClient.on("message", function(message)
+            {
+                var messageParsed = JSON.parse(message);
+                if(messageParsed.command.name == expectedCommand ){
+                    json = messageParsed;
+                }
+            });
+
+            //Assert
+
+            setTimeout(function()
+            {
+                assert.equal(expectedCommand, json.command.name);
+                done();
+            }, 2000);
+
+        });
+
     });
 
     describe('keepAlive', function()
@@ -516,7 +688,256 @@ describe("Functionnal webSocket", function ()
 
     });
 
+    describe('updateUserLevel', function()
+    {
+        var tokenSecret = process.env.TOKEN_SECRET || 'FRIMA_TOKEN_SECRET';
+        var registerClient;
+        var updateUserLevel;
+        var user;
+
+        var EXPERIENCE_POINTS = 1000;
+        var LEVEL = 2;
+        var EXPECTED_ATTACK_POWER_LEVEL = 2;
+        var EXPECTED_STAMINA_POWER_LEVEL = 2;
+        var EXPECTED_HYPE_POWER_LEVEL = 2;
+
+        beforeEach(function (done)
+        {
+            updateUserLevel = {
+                "command":
+                {
+                    "name": "updateUserLevel",
+                    "parameters":
+                    {
+                        "experiencePoints": EXPERIENCE_POINTS,
+                        "level": LEVEL,
+                        "attackPowerLevelUpgrade": 1,
+                        "staminaPowerLevelUpgrade": 1,
+                        "hypePowerLevelUpgrade": 1
+                    }
+                }
+            };
+
+            user = new User();
+            user.id = 123456789;
+            user.username = "Test";
+            user.email = "test@test.ca";
+            user.items = [];
+            user.experiencePoints = 100;
+            user.upgradePointsOnLevelComplete = 2;
+            user.requiredExperiencePointsForNextLevel = 100;
+            user.level = 1;
+            user.attackPowerLevel = 1;
+            user.staminaPowerLevel = 1;
+            user.hypePowerLevel = 1;
 
 
+            var expires = moment().add(1, 'days').valueOf();
+            user.token = jwt.encode(
+                {
+                    iss: user.id,
+                    exp: expires
+                },
+                tokenSecret
+            );
+
+            registerClient =
+            {
+                "command" : {
+                    "name" : "registerClient",
+                    "parameters" : {
+                        "token" : user.token
+                    }
+                }
+            };
+
+            user.save(function(err){
+                if(err){
+                    console.log("Error to save user in functionnal test: ", err);
+                }
+                done();
+            });
+
+        });
+
+        afterEach(function(done){
+            User.remove({ }, function (err) {
+                if (err){
+                    console.log("Problem to Remove user!: ", err);
+                }
+
+                done();
+            });
+        });
+
+        it('should receive userLevelUpInformation', function(done)
+        {
+            //Arrange
+            this.timeout(5000);
+
+            //Act
+            webSocketClient.onopen = function()
+            {
+                webSocketClient.send(JSON.stringify(registerClient));
+                webSocketClient.send(JSON.stringify(updateUserLevel));
+            };
+
+            var expectedCommand = "userLevelUpInformation";
+            var json;
+
+            webSocketClient.on("message", function(message) {
+                var messageParsed = JSON.parse(message);
+                if(messageParsed.command.name == expectedCommand){
+                    json = messageParsed;
+                }
+            });
+
+            //Assert
+            setTimeout(function() {
+                assert.equal(expectedCommand, json.command.name);
+
+                done();
+            }, 1000);
+
+        });
+
+        it('should update user level and properties', function(done)
+        {
+            //Arrange
+            this.timeout(5000);
+
+            //Act
+            webSocketClient.onopen = function()
+            {
+                webSocketClient.send(JSON.stringify(registerClient));
+                webSocketClient.send(JSON.stringify(updateUserLevel));
+            };
+
+            setTimeout(function(){
+
+                var decoded = jwt.decode(user.token, 'FRIMA_TOKEN_SECRET');
+                User.findOne({ '_id': decoded.iss }, function (err, updatedUser)
+                {
+                    if (!err)
+                    {
+                        if (updatedUser)
+                        {
+                            //Assert
+                            assert.equal(EXPERIENCE_POINTS, updatedUser.experiencePoints);
+                            assert.equal(LEVEL, updatedUser.level);
+                            assert.equal(EXPECTED_ATTACK_POWER_LEVEL, updatedUser.attackPowerLevel);
+                            assert.equal(EXPECTED_HYPE_POWER_LEVEL, updatedUser.hypePowerLevel);
+                            assert.equal(EXPECTED_STAMINA_POWER_LEVEL, updatedUser.staminaPowerLevel);
+                            done();
+                        }
+                    }
+                });
+
+            }, 3000);
+
+        });
+
+    });
+
+    describe('updateUserExperience', function()
+    {
+        var tokenSecret = process.env.TOKEN_SECRET || 'FRIMA_TOKEN_SECRET';
+        var registerClient;
+        var updateUserExperience;
+        var user;
+
+        var EXPERIENCE_POINTS = 1000;
+
+        beforeEach(function (done)
+        {
+            updateUserExperience = {
+                "command":
+                {
+                    "name": "updateUserExperience",
+                    "parameters":
+                    {
+                        "experiencePoints": EXPERIENCE_POINTS
+                    }
+                }
+            };
+
+            user = new User();
+            user.id = 123456789;
+            user.username = "Test";
+            user.email = "test@test.ca";
+            user.items = [];
+            user.experiencePoints = 100;
+
+            var expires = moment().add(1, 'days').valueOf();
+            user.token = jwt.encode(
+                {
+                    iss: user.id,
+                    exp: expires
+                },
+                tokenSecret
+            );
+
+            registerClient =
+            {
+                "command" : {
+                    "name" : "registerClient",
+                    "parameters" : {
+                        "token" : user.token
+                    }
+                }
+            };
+
+            user.save(function(err){
+                if(err){
+                    console.log("Error to save user in functionnal test: ", err);
+                }
+                done();
+            });
+
+        });
+
+        afterEach(function(done){
+            User.remove({ }, function (err) {
+                if (err){
+                    console.log("Problem to Remove user!: ", err);
+                }
+
+                done();
+            });
+        });
+
+        it('should update user experience', function(done)
+        {
+            //Arrange
+            this.timeout(5000);
+
+            //Act
+            webSocketClient.onopen = function()
+            {
+                webSocketClient.send(JSON.stringify(registerClient));
+                webSocketClient.send(JSON.stringify(updateUserExperience));
+            };
+
+            setTimeout(function(){
+
+                var decoded = jwt.decode(user.token, 'FRIMA_TOKEN_SECRET');
+                User.findOne({ '_id': decoded.iss }, function (err, updatedUser)
+                {
+                    if (!err)
+                    {
+                        if (updatedUser)
+                        {
+                            //Assert
+                            assert.equal(EXPERIENCE_POINTS, updatedUser.experiencePoints);
+                            done();
+                        }
+                    }
+                });
+
+            }, 3000);
+
+        });
+
+    });
 
 });
